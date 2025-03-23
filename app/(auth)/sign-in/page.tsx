546d234807d42cd9ff2e5437cb51105037310a8f
Hustler-Sameer <sameerkurkure10@gmail.com>
2025-03-23 16:18:54 +0530

    added auth form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+import AuthForm from "@/components/forms/AuthForm";
+import { SignInSchema } from "@/lib/validations";
 import React from "react";
 
 const signIn = () => {
-  return <div>Signin</div>;
+  return (
+    <AuthForm
+      formType="SIGN_IN"
+      schema={SignInSchema}
+      defaultValues={{ email: "", password: "" }}
+      onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
+  );
 };
 
 export default signIn;
